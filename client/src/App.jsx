39f444d24e0fb98c6c0303aee132fe61902067f9
jinilptl <jrpatel1747@gmail.com>
2025-11-24 15:27:47 +0530

    fix: Correct project route path and clean up import statements in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,13 @@
-
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/Homepage/HomePage'
-import ForgotPasswordPage from './pages/ForgotPassword/ForgotPasswordPage'
-import ResetPasswordPage from './pages/ForgotPassword/ResetPasswordPage'
-import { useSelector } from 'react-redux'
-import Navigation from './components/Navigation'
-import Dashboard from './components/Dashboard'
-import DashboardPage from './pages/DashBoard/DashboardPage'
-import TeamsPage from './pages/DashBoard/TeamsPage'
-import ProjectPage from './pages/DashBoard/ProjectPage'
-
-
-import Dashboard from "./components/Dashboard";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Homepage/HomePage";
+import ForgotPasswordPage from "./pages/ForgotPassword/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ForgotPassword/ResetPasswordPage";
+import { useSelector } from "react-redux";
 import DashboardPage from "./pages/DashBoard/DashboardPage";
+import Dashboard from "./components/Dashboard";
+import ProjectPage from "./pages/DashBoard/ProjectPage";
+
 import TeamsPage from "./pages/DashBoard/TeamsPage";
 
 import ProtectedWrapper from "./components/Wrappers/ProtectedWrapper";
@@ -39,7 +33,7 @@ const App = () => {
         >
           <Route index element={<Dashboard />} />
           <Route path="/dashboard/teams" element={<TeamsPage />} />
-          <Route path='/dashboard/project' element={<ProjectPage />} />
+          <Route path="/dashboard/projects" element={<ProjectPage/>} />
         </Route>
 
         <Route path="*" element={<NotFoundPage />} />
