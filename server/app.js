bf2633ffb984961502d8232e7bbdb50094349c18
jinilptl <jrpatel1747@gmail.com>
2025-12-19 16:48:02 +0530

    feat: Add file upload router and dependencies for handling file uploads

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import authRouter from './routes/auth.routes.js';
 import teamRouter from './routes/team.routes.js';
 import projectRouter from './routes/project.routes.js';
 import taskRouter from './routes/tasksRoutes/task.routes.js';
+import fileUploadRouter from "./routes/tasksRoutes/fileUpload.routes.js"
 
 const app= express();
 
@@ -24,6 +25,7 @@ app.use("/api/v1/user",authRouter)
 app.use("/api/v1/team",teamRouter)
 app.use("/api/v1/project",projectRouter)
 app.use("/api/v1/project/task",taskRouter)
+app.use("/api/v1/project/task",fileUploadRouter)
 
 
 // error handler global formate
