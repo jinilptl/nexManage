a7204279e3f03e38176253efbac5170bd7599f6b
Chintan9094 <161949703+Chintan9094@users.noreply.github.com>
2026-02-18 11:49:46 +0530

    feat: add ProtectedWrapper component to secure routes by validating JWT tokens and redirecting unauthenticated users.

--- client/src/components/Wrappers/ProtectedWrapper.jsx
+++ client/src/components/Wrappers/ProtectedWrapper.jsx
@@ -5,10 +5,7 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedWrapper = ({ children }) => {
   const token = useSelector((state) => state.auth.token);
-
   if (!token) {
-    toast.error("Please login to access this page");
-
     return <Navigate to="/" replace />;
   }
 
